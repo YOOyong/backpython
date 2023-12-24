45bc525874b7621db24272e61180cd79551ed8dd
YOOyong <phg5656@gmail.com>
2023-12-24 21:17:42 +0900

    함수 분리

--- api/app.py
+++ api/app.py
@@ -1,6 +1,69 @@
-from flask import Flask, jsonify ,request
+from flask import Flask, jsonify ,request, current_app
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
+
+class CustomJSONEndocer(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj,set):
+            return list(obj)
+        return super().default(obj)
+
+
+def get_user(user_id):
+    user = current_app.database.execute(text("""
+        select id, name, email, profile
+        from users
+        where id = :user_id
+    """),{'user_id':user_id}).fetchone()
+
+    return {'id' : user['id'],
+            'name': user['name'],
+            'email': user['email'],
+            'profile': user['profile']
+            } if user else None
+
+
+def insert_tweet(new_tweet):
+    return current_app.database.execute(text("""
+    insert into tweets (user_id, tweet)
+    values (:id, :tweet)   
+    """),new_tweet).rowcount
+
+def insert_user(new_user):
+    return current_app.database.execute(text("""
+    insert into users (name, email, profile, hashed_password)
+    values(:name, :email, :profile ,:password)
+    """), new_user).lastrowid
+
+def insert_follow(user_follow):
+    return current_app.database.execute(text("""
+    insert into users_follow_list(user_id, follow_user_id)
+    values(:id , :follow)
+    """),user_follow).rowcount
+
+
+def delete_follow(user_unfollow):
+    return current_app.database.execute(text("""
+    delete from users_follow_list
+    where user_id = :id
+    and follow_user_id = :unfollow
+    """), user_unfollow).rowcount
+
+
+def get_timeline(user_id):
+    timeline = current_app.database.execute(text("""
+    select t.user_id, t.tweet
+    from tweets t
+    left join users_follow_list ufl on ufl.user_id = :user_id
+    where t.user_id = :user_id
+    or t.user_id = ufl.follow_user_id
+    """),{'user_id' : user_id}).fetchall()
+
+    return [{
+        'user_id': tweet['user_id'],
+        'tweet' : tweet['tweet']
+    } for tweet in timeline]
+
 
 # 이 안에 엔드포인트들을 구현한다.
 # create_app을 팩토리함수로 자동으로 인식하여 flask를 실행시킨다.
@@ -18,85 +81,40 @@ def create_app(test_config = None):
     @app.route('/signup', methods = ['POST'])
     def signup():
         new_user = request.json
-        new_user_id = app.database.execute(text("""
-                            insert into users (
-                                name, email, profile, hashed_password
-                            ) values (
-                                :name,
-                                :email,
-                                :profile,
-                                :password
-                            )"""), new_user).lastrowid
-        
-        row = app.database.execute(text("""
-        select * from users
-        where id = :id
-        """), {'id' : new_user_id}).fetchone()
-
-
-        created_user_id = {
-            'id' : row['id'],
-            'name' : row['name'],
-            'email' : row['email'],
-            'profile' : row['profile']
-        } if row else None
-
+        new_user_id = insert_user(new_user)
+        created_user_id = get_user(new_user_id)
         return jsonify(created_user_id)
 
     @app.route('/tweet', methods = ['POST'])
     def tweet():
         new_tweet = request.json
         
-        if not app.database.execute(text("select * from users where id = :id"),\
-                                     {'id': new_tweet['id']}).fetchone():
-            return '없는 유저입니다.' , 400
+        if not get_user(new_tweet['id']):
+            return '없는 유저입니다.', 400
 
         if len(new_tweet['tweet']) > 300:
             return '300 자를 초과', 400
 
-
-        app.database.execute(text("""
-            insert into tweets (user_id, tweet)
-                values (:id, :tweet)                           
-        """), new_tweet)
+        insert_tweet(new_tweet)
 
         return '', 200
     
 
     @app.route('/timeline/<int:user_id>', methods = ['GET'])
     def timeline(user_id):
-        rows = app.database.execute(text("""
-            select t.user_id, t.tweet
-            from tweets t
-            left join users_follow_list ufl on ufl.user_id = :id
-            where t.user_id = :id
-            or t.user_id = ufl.follow_user_id
-        """), {'id' : user_id}).fetchall()
 
-        timeline = [
-            {
-                'user_id' : row['user_id'],
-                'tweet' : row['tweet']
-            } for row in rows
-        ]
+        timeline = get_timeline(user_id)
 
         return jsonify(timeline)
 
     @app.route('/follow', methods = ['POST'])
     def follow():
-        following = request.json
+        requests = request.json
 
-        if not app.database.execute(text("select * from users where id = :id"),\
-                                     {'id': following['id']}).fetchone() \
-            or not app.database.execute(text("select * from users where id = :id"),{'id' : following['follow']}).fetchone():
+        if not get_user(requests['id']) or not get_user(requests['follow']):
             return '없는 유저입니다.' , 400
 
-        app.database.execute(text("""
-            insert into users_follow_list (user_id, follow_user_id)
-            values(:id, :follow)
-            """
-        ), following)
-
+        insert_follow(requests)
 
         return '', 200
     
@@ -104,9 +122,7 @@ def create_app(test_config = None):
     def unfollow():
         requests = request.json
 
-        if not app.database.execute(text("select * from users where id = :id"),\
-                                     {'id': requests['id']}).fetchone() \
-            or not app.database.execute(text("select * from users where id = :id"),{'id' : requests['unfollow']}).fetchone():
+        if not get_user(requests['id']) or not get_user(requests['unfollow']):
             return '없는 유저입니다.' , 400
 
         app.database.execute(text("""
@@ -116,9 +132,9 @@ def create_app(test_config = None):
             """
         ), requests)
 
+        delete_follow(requests)
 
         return '', 200
-
 
     return app
 
